ab56d6c32b72d1eb0bd2cd58dd8eb0e871450abd
Jon Wong-Lam <jonwonglam@gmail.com>
2017-03-09 14:38:14 -0800

    Add tooltips when hovering over markers

--- src/js/map.js
+++ src/js/map.js
@@ -18,22 +18,80 @@ function initMap() {
 
 function setMarkers() {
   var bounds = new google.maps.LatLngBounds();
+  var largeInfowindow = new google.maps.InfoWindow();
+  var hoverInfowindow = new google.maps.InfoWindow();
+
+  var locationIcon = {
+        path: 'M10,2A5,5,0,0,0,5,7c0,4.77,5,11,5,11s5-6.23,5-11A5,5,0,0,0,10,2Z',
+        fillColor: '#000',
+        fillOpacity: 0.8,
+        scale: 1.8,
+        strokeColor: '#484e56',
+        strokeWeight: 1
+      };
+
+  var result;
 
   for (var i = 0; i < viewModal.resultList().length; i++) {
-    var position = viewModal.resultList()[i].location;
-    var title = viewModal.resultList()[i].title;
-
+    // Create a local copy for easy access
+    result = viewModal.resultList()[i];
+    // Create a new marker for each result
     var marker = new google.maps.Marker({
       map: map,
-      position: position,
-      title: title,
+      position: result.location,
+      name: result.title,
+      imgSrc: result.imgSrc,
+      address: result.address,
+      city: result.city,
+      checkins: result.checkins,
+      rating: result.rating,
+      url: result.url,
+      icon: locationIcon,
       animation: google.maps.Animation.Drop,
       id: i
     });
 
+    // Add marker to the list
     markers.push(marker);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+
+    });
+    marker.addListener('mouseover', function() {
+      populateInfoWindow(this, hoverInfowindow, 'mouseout');
+    });
     bounds.extend(markers[i].position);
   }
   // Extend the boundaries of the map for each marker
   map.fitBounds(bounds);
+}
+
+function populateInfoWindow(marker, infoWindow, onCloseEvent) {
+  if (infoWindow.marker == null) {
+    infoWindow.marker = marker;
+    infoWindow.setContent(
+      '<div class=" d-flex">' +
+        '<img class="result-img" src="' + marker.imgSrc + '">' +
+        '<div class="result-text-container">' +
+          '<h3 class="result-title">' + marker.name + '</h3>' +
+          '<h4 class="result-subtitle">' + marker.address + '</h4>' +
+          '<h4 class="result-subtitle">' + marker.city + '</h4>' +
+          '<div class="result-details d-flex align-items-center">' +
+            '<span class="result-icon"><i class="fa fa-star" aria-hidden="true"></i></span>' +
+            '<h5>' + marker.checkins + '</h5>' +
+            '<span class="result-icon"><i class="fa fa-blind" aria-hidden="true"></i></span>' +
+            '<h5>' + '6.2 miles round trip' + '</h5>' +
+          '</div>' +
+        '<div>' +
+        '<div class="rating">' + marker.rating + '</div>' +
+      '</div>' + '</div>' + '</div>' +
+      '<div class="result-link">' + '<a href="' + marker.url + '">See on Foursquare</a>' + '</div>'
+    );
+    infoWindow.open(map, marker);
+
+    marker.addListener('mouseout', function() {
+      // infoWindow.close();
+      infoWindow.marker = null;
+    });
+  }
 }
